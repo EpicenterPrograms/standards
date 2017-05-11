03f15ec345f4143c23634ddc391d8f3d8916b009
Robert <ambitiousbusinessowner@gmail.com>
2017-05-11 02:04:47 -0400

    hopefully made this script finish in Safari

--- behavior.js
+++ behavior.js
@@ -100,7 +100,8 @@ Standards.queue.add = function(object) {
     }
 };
 
-Standards.audio = new window.AudioContext() || window.webkitAudioContext();  // used in Sound()
+Standards.audio = new (window.AudioContext()!=undefined ? window.AudioContext() : window.webkitAudioContext());  // used in Sound()
+// Safari is dumb and doesn't like the way I normally assign defaults to variables, so I did this way here, so the script would at least finish.
 // Standards.audio.close() gets rid of the instance (if you used multiple instances, you'd max out at around 6)
 
 var Sound = function(specs) {
