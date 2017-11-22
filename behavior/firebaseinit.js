e879d7d5e2dbe72fbd6749c13ad26c38c68ed993
Robert <ambitiousbusinessowner@gmail.com>
2017-11-22 17:53:44 -0600

    added a .js file extension

---
+++ behavior/firebaseinit.js
@@ -0,0 +1,43 @@
+if (Standards) {
+	if (typeof Standards != "object") {
+		var Standards = {};
+		console.warn("typeof Standards is not an object");
+	}
+} else {
+	var Standards = {};
+}
+if (Standards.options) {
+	if (typeof Standards.options != "object") {
+		Standards.options = {};
+		console.warn("typeof Standards.options is not an object");
+	}
+} else {
+	Standards.options = {};
+};
+if (Standards.options.serverSettings) {
+	if (typeof Standards.options.serverSettings != "object") {
+		Standards.options.serverSettings = {
+			apiKey: "AIzaSyDWDEU1NcHtuAi-Fg-ahu-o1CRHwAhVo_s",
+			authDomain: "epicenterresources.firebaseapp.com",
+			databaseURL: "https://epicenterresources.firebaseio.com",
+			projectId: "epicenterresources",
+			storageBucket: "",
+			messagingSenderId: "705640376450"
+		};
+		console.warn("typeof Standards.options.serverSettings is not an object");
+	}
+} else {
+	Standards.options.serverSettings = {
+		apiKey: "AIzaSyDWDEU1NcHtuAi-Fg-ahu-o1CRHwAhVo_s",
+		authDomain: "epicenterresources.firebaseapp.com",
+		databaseURL: "https://epicenterresources.firebaseio.com",
+		projectId: "epicenterresources",
+		storageBucket: "",
+		messagingSenderId: "705640376450"
+	};
+};
+
+if (!(Standards.options.serverSettings.initialization == "none" || Standards.options.serverSettings.initialization == false)) {
+	// initializes Firebase
+	firebase.initializeApp(Standards.options.serverSettings);
+}
