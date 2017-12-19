230f7ee21389b446e5120b4e381f80046dda4c75
Robert <ambitiousbusinessowner@gmail.com>
2017-12-19 02:07:44 -0600

    established a new order
    * put the general scripting into the "behavior" folder
    * refined some behavior
    * changed the standards reference assignment
    * refined some formatting
    * updated the website
    * added some content to the README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Standards
+This is where I host a bunch of scripting and styling standards. If you decide to use this, it is strongly encouraged that you make your own copy since this is highly susceptible to change. Feel free to make pull requests.
--- behavior/firebaseinit.js
+++ behavior/firebaseinit.js
@@ -1,22 +1,31 @@
 if (Standards) {
-	if (typeof Standards != "object") {
+	if (!(Standards instanceof Object)) {
 		var Standards = {};
 		console.warn("typeof Standards is not an object");
 	}
 } else {
 	var Standards = {};
 }
-if (Standards.options) {
-	if (typeof Standards.options != "object") {
-		Standards.options = {};
-		console.warn("typeof Standards.options is not an object");
+if (Standards.firebaseinit) {
+	if (!(Standards.firebaseinit instanceof Object)) {
+		Standards.firebaseinit = {};
+		console.warn("typeof Standards.firebaseinit is not an object");
 	}
 } else {
-	Standards.options = {};
-};
-if (Standards.options.serverSettings) {
-	if (typeof Standards.options.serverSettings != "object") {
-		Standards.options.serverSettings = {
+	Standards.firebaseinit = {};
+}
+if (Standards.firebaseinit.options) {
+	if (!(Standards.firebaseinit.options instanceof Object)) {
+		Standards.firebaseinit.options = {};
+		console.warn("typeof Standards.firebaseinit.options is not an object");
+	}
+} else {
+	Standards.firebaseinit.options = {};
+}
+
+if (Standards.firebaseinit.options.serverSettings) {
+	if (typeof Standards.firebaseinit.options.serverSettings != "object") {
+		Standards.firebaseinit.options.serverSettings = {
 			apiKey: "AIzaSyDWDEU1NcHtuAi-Fg-ahu-o1CRHwAhVo_s",
 			authDomain: "epicenterresources.firebaseapp.com",
 			databaseURL: "https://epicenterresources.firebaseio.com",
@@ -24,10 +33,10 @@ if (Standards.options.serverSettings) {
 			storageBucket: "",
 			messagingSenderId: "705640376450"
 		};
-		console.warn("typeof Standards.options.serverSettings is not an object");
+		console.warn("typeof Standards.firebaseinit.options.serverSettings is not an object");
 	}
 } else {
-	Standards.options.serverSettings = {
+	Standards.firebaseinit.options.serverSettings = {
 		apiKey: "AIzaSyDWDEU1NcHtuAi-Fg-ahu-o1CRHwAhVo_s",
 		authDomain: "epicenterresources.firebaseapp.com",
 		databaseURL: "https://epicenterresources.firebaseio.com",
@@ -35,9 +44,9 @@ if (Standards.options.serverSettings) {
 		storageBucket: "",
 		messagingSenderId: "705640376450"
 	};
-};
+}
 
-if (!(Standards.options.serverSettings.initialization == "none" || Standards.options.serverSettings.initialization == false)) {
+if (!(Standards.firebaseinit.options.serverSettings.initialization == "none" || Standards.firebaseinit.options.serverSettings.initialization == false)) {
 	// initializes Firebase
-	firebase.initializeApp(Standards.options.serverSettings);
+	firebase.initializeApp(Standards.firebaseinit.options.serverSettings);
 }
