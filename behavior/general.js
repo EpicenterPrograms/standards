ce711ffca5825e73ac1519de4152fdbeab46c6d1
Robert <ambitiousbusinessowner@gmail.com>
2019-04-22 08:10:12 -0500

    tried to force the code to tell me what's going on

--- behavior/general.js
+++ behavior/general.js
@@ -3676,9 +3676,8 @@ Standards.general.storage.server = {
 			alert("There's no server to handle this action.");
 			throw "Firebase or Firestore doesn't exist.";
 		}
-		console.log(window.location.href.protocol);
 		if (window.location.href.protocol != "http:" && window.location.href.protocol != "https:") {
-			alert("Access to the server isn't allowed from this URL.");
+			alert("Access to the server isn't allowed from this URL. (" + window.location.href.protocol + ")");
 			throw 'The URL doesn\'t use the protocol "http" or "https".';
 		}
 		if (!shouldNotCheckUser && !Standards.general.storage.server.user) {
