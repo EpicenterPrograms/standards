8099889062234905ca24e2811f9eec24db3e1e4c
Robert <ambitiousbusinessowner@gmail.com>
2025-06-22 23:19:04 -0500

    tried out a new Firebase initialization scheme

--- behavior/firebaseinit.js
+++ behavior/firebaseinit.js
@@ -1,59 +1,38 @@
-/*
+/*  This is being updated from 4.6.1 to 11.9.1. It now needs to use the "module" type in the script import.
 This assumes you have previously imported the scripts:
 https://www.gstatic.com/firebasejs/4.6.1/firebase.js
 https://www.gstatic.com/firebasejs/4.6.1/firebase-firestore.js
 and are making use of the project with the ID "epicenterresources"
 */
 
-if (Standards) {
-	if (!(Standards instanceof Object)) {
-		var Standards = {};
-		console.warn("typeof Standards is not an object");
-	}
-} else {
-	var Standards = {};
-}
-if (Standards.firebaseinit) {
-	if (!(Standards.firebaseinit instanceof Object)) {
-		Standards.firebaseinit = {};
-		console.warn("typeof Standards.firebaseinit is not an object");
-	}
-} else {
-	Standards.firebaseinit = {};
-}
-if (Standards.firebaseinit.options) {
-	if (!(Standards.firebaseinit.options instanceof Object)) {
-		Standards.firebaseinit.options = {};
-		console.warn("typeof Standards.firebaseinit.options is not an object");
-	}
-} else {
-	Standards.firebaseinit.options = {};
-}
+import firebase from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app-compat.js";  // newer Firebase that works like the old one
+import "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore-compat.js";  // modifies Firebase to include Firestore (in the old way)
+window.firebase = firebase;  // makes the firebase object globally available (like it originally was)
+import {  // imports modern authentication to prevent failures from using the old version
+	getAuth,
+	onAuthStateChanged,
+	GoogleAuthProvider,
+	signInWithRedirect,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
+	signInAnonymously,
+	signOut
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
 
-if (Standards.firebaseinit.options.serverSettings) {
-	if (typeof Standards.firebaseinit.options.serverSettings != "object") {
-		Standards.firebaseinit.options.serverSettings = {
-			apiKey: "AIzaSyDWDEU1NcHtuAi-Fg-ahu-o1CRHwAhVo_s",
-			authDomain: "epicenterresources.firebaseapp.com",
-			databaseURL: "https://epicenterresources.firebaseio.com",
-			projectId: "epicenterresources",
-			storageBucket: "",
-			messagingSenderId: "705640376450"
-		};
-		console.warn("typeof Standards.firebaseinit.options.serverSettings is not an object");
-	}
-} else {
-	Standards.firebaseinit.options.serverSettings = {
-		apiKey: "AIzaSyDWDEU1NcHtuAi-Fg-ahu-o1CRHwAhVo_s",
-		authDomain: "epicenterresources.firebaseapp.com",
-		databaseURL: "https://epicenterresources.firebaseio.com",
-		projectId: "epicenterresources",
-		storageBucket: "",
-		messagingSenderId: "705640376450"
-	};
-}
+window.Standards ??= {};
+Standards.firebaseinit ??= {};
+Standards.firebaseinit.options ??= {};
+Standards.firebaseinit.options.serverSettings ??= {
+	apiKey: "AIzaSyDWDEU1NcHtuAi-Fg-ahu-o1CRHwAhVo_s",
+	authDomain: "epicenterresources.firebaseapp.com",
+	databaseURL: "https://epicenterresources.firebaseio.com",
+	projectId: "epicenterresources",
+	storageBucket: "",
+	messagingSenderId: "705640376450",
+	initialization: true
+};
 
-if (!(Standards.firebaseinit.options.serverSettings.initialization == "none" || Standards.firebaseinit.options.serverSettings.initialization == false)) {
+if (!(Standards.firebaseinit.options.serverSettings.initialization === "none" || Standards.firebaseinit.options.serverSettings.initialization === false)) {
 	// initializes Firebase
 	firebase.initializeApp(Standards.firebaseinit.options.serverSettings);
 }
